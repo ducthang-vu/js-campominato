c414a310c4e918b3c9e6bc6a88a46b701e517829
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-06 15:21:41 +0200

    fourth function completed

--- script/main.js
+++ script/main.js
@@ -38,15 +38,23 @@ function randomNumberSet(n, max) {
 
 
 function promtpUser(max, invalid_array) {
-    //A function accepting a integer max and an array 'invalid_array', and prompting user to enter a number, as many times as necessary until 1 < number <= max and != from any item of 'invalid_array'. Returns the valid number.
+    //A function accepting an integer 'max' and an array 'invalid_array', and prompting user to enter a number 'user_value', as many times as necessary until 1 < 'user_value' <= max and != from any item of 'invalid_array'. Returns the 'user_value'.
     var user_value = parseInt(prompt('Enter a number between 1 and ' + max))
     while (isNaN(user_value) || user_value < 1 || user_value > max || invalid_array.includes(user_value)) {
-        user_value = parseInt(prompt('Your number is not valid, enter a diferrent number: '))
+        user_value = parseInt(prompt('Your number is not valid, enter a diferrent number between 1 and ' + max + ': '))
     }
 
     return user_value
 }
 
+
+function resultOfAttempt(user_value, invalid_set) {
+    //A function accepting a number 'user_value' and a set 'invalid_set'; and returing true if 'user_value' is not an element of the set.
+    if (invalid_set.has(user_value)) {
+        return false    //game over
+    }
+    return true //game continue
+}
 
 
 function gameplay(userChoise) {
@@ -62,11 +70,18 @@ function gameplay(userChoise) {
     //creating array of attempt
     var attempted = []
 
-    //ask user for number
-    var singleAttempt = parseInt(prompt('Enter a number:'))
-        //validate the number
+    //ask user for number and validate number
+    var singleAttempt = promtpUser(levelMax, attempted)
+
 
     //if number is in set
+    var resultAttempt = resultOfAttempt(singleAttempt, losingNumbers)
+
+    }
+
         //user loses
+            //print score
         //game continue
+            //updated attempted array
+            //update score
 }
